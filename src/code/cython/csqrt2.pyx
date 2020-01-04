269d53ad7cc2c465bc55938125b55a3084d8fde7
cnbluegeek <cnbluegeek@gmail.com>
2020-01-04 13:38:26 +0800

    cython and revise

---
+++ src/code/cython/csqrt2.pyx
@@ -0,0 +1,14 @@
+# cython: language_level=3
+from libc.math cimport sqrt
+import numpy as np
+
+def csqrt(double[:,:] X):
+    cdef int r = X.shape[0]
+    cdef int c = X.shape[1]
+    cdef double[:, :] Y = np.zeros((r, c))
+    cdef int i, j
+
+    for i in range(r):
+        for j in range(c):
+            Y[i, j] = sqrt(X[i, j])
+    return Y  
